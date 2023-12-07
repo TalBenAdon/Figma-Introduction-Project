e38ad01b54d1b1cb206a1142929f22d26837c297
Tal Ben Adon <talbenadon2@gmail.com>
2023-12-08 00:07:41 +0200

    first stage complete

--- src/Articles/Article.jsx
+++ src/Articles/Article.jsx
@@ -1,11 +1,20 @@
 
 import React from 'react'
+import './Article.css'
 
 export default function Article(props) {
+    const { coverImage, date, title, intro } = props
     return (
-        <div>
 
+        <div className='article-card-container'>
 
+            <a href="" className='article-card-img'><img src={coverImage} alt="coverimg" />
+            </a>
+            <div className='article-card-date'>
+                {date}
+            </div>
+            <h3>{title}</h3>
+            <p>{intro}</p>
         </div>
     )
 }
--- src/List/List.jsx
+++ src/List/List.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Article from '../Articles/Article'
 import Posts from "../../public/posts.json"
+import './List.css'
 
 export default function List() {
     return (
-        <div>
-            <div>Popular Topics</div>
-            <div>
+        <div className='grid-wrapper'>
+            <h2>Popular Topics</h2>
+            <div className='big-list-container'>
                 {Posts.map(post => {
                     return <Article key={post.title} coverImage={post.coverImage} title={post.title} link={post.link} tags={post.tags} date={post.date} intro={post.intro} />
                 })}
